ab93e0d376b5cd5a95da5625b66eefccb2c506cc
tia-nn <yknms3533642@yahoo.co.jp>
2023-06-11 16:45:50 +0900

    bash_completions.bash add linux support

--- xonsh/bash_completions.bash
+++ xonsh/bash_completions.bash
@@ -6,3 +6,11 @@ fi
 if [ -e /opt/git-completion/git-completion.bash ]; then
     source /opt/git-completion/git-completion.bash
 fi
+
+# for linux
+
+if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+fi
