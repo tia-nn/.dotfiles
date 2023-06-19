0f25dcf3a7658dfebe9111808c0b7a0205fd88ef
tia-nn <yknms3533642@yahoo.co.jp>
2023-06-20 08:27:17 +0900

    add vscode config

--- installs/configs.sh
+++ installs/configs.sh
@@ -9,3 +9,15 @@ git config --global diff.indentHeuristic true
 
 ## npm
 npm config -g set prefix=$HOME/.npm
+
+
+## vscode  see also https://code.visualstudio.com/docs/getstarted/settings#_settings-file-locations
+if [ "$(uname)" == 'Darwin' ]; then
+    vscode_profile="$HOME/Library/Application\ Support/Code/User/settings.json"
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    vscode_profile="$HOME/.config/Code/User/settings.json"
+else
+    echo "not supported your platform..." >&2
+    exit 1
+fi
+mv ${vscode_profile} $(dirname $vscode_profile)/settings.old.json
